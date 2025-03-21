5d1a1644e038e3e59ea2d542f894f79fccd434c6
songlongdaiduong <song.long.dai.duong@gmail.com>
2025-03-21 14:35:32 +0800

    fonts: Add Exotwo font

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
     AOSPASettingsProviderOverlay \
     AOSPASystemUIOverlay \
     CertifiedPropsOverlay \
+    FontExotwoOverlay \
     FontGoogleSansOverlay \
     FontHarmonySansOverlay \
     FontInterOverlay \
