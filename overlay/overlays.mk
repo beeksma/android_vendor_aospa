063355f724fddad98555f0f7fd36243f0ccea6a0
songlongdaiduong <song.long.dai.duong@gmail.com>
2025-06-05 18:51:41 +0800

    Revert "overlay: Move certified props to a separate RRO"
    This reverts commit a5445e226941b06d6a36ef4aaaddc5f9e9ba4263.

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -23,7 +23,6 @@ PRODUCT_PACKAGES += \
     AOSPASettingsOverlay \
     AOSPASettingsProviderOverlay \
     AOSPASystemUIOverlay \
-    CertifiedPropsOverlay \
     FontComfortaaOverlay \
     FontExotwoOverlay \
     FontGoogleSansOverlay \
@@ -79,6 +78,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aospa/overlay/static
 PRODUCT_PACKAGE_OVERLAYS += vendor/aospa/overlay/static
-
-PRODUCT_COPY_FILES += \
-    vendor/aospa/overlay/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
